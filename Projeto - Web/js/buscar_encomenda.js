5b86ac9e2baa3fd62e181769940cf35e8899636b
devgustacoosta <161398422+devgustacoosta@users.noreply.github.com>
2024-05-24 10:32:38 -0300

    Add files via upload

---
+++ Projeto - Web/js/buscar_encomenda.js
@@ -0,0 +1,17 @@
+var botaoBusca = document.querySelector("#buscar-encomendas");
+
+botaoBusca.addEventListener("click", function(){
+    var xhr = new XMLHttpRequest();
+    
+    xhr.open("GET", "http://localhost:3000/encomendas");
+    xhr. addEventListener("load", function(){
+        var resposta = xhr.responseText;
+        var encomendas = JSON.parse(resposta);
+            
+        encomendas.forEach(function(cada_encomenda){
+            adicionaEncomenda(cada_encomenda);
+        });
+    });
+    xhr.send();
+});
+
---
+++ Projeto - Web/js/pop-up.js
@@ -0,0 +1,12 @@
+function popForm() {
+    var width = 600;
+    var height = 400;
+    var left = (window.innerWidth - width) / 2;
+    var top = (window.innerHeight - height) / 2;
+
+    var popupWindow = window.open('pop-form.html', 'Pop-up', 'width=' + width + ', height=' + height + ', top=' + top + ', left=' + left + ', resizable=no, scrollbars=no');
+
+    if (window.focus) {
+        popupWindow.focus();
+    }
+}
